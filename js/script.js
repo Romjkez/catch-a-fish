3ae1ef35b9f1b77a7b025698cd638b43e4e657b1
Romka023 <meshkov.ra@yandex.ru>
2018-08-04 22:34:49 +0300

    added Start button, added falling stone

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 var fish1 = document.getElementById('fish1'),
     fish2 = document.getElementById('fish2'),
-    fish3 = document.getElementById('fish3');
-
+    fish3 = document.getElementById('fish3'),
+    btn = document.getElementById('btn'),
+    preview = document.getElementById('preview');
+document.addEventListener('DOMContentLoaded',initial);
+function initial() {
+    document.addEventListener('click',closePreview);
+}
+function closePreview() {
+    preview.animate([{top:0},{top:'-1500px'}],{duration:1000,fill:'both',easing:'ease'});
+    document.getElementById('fish5').style.animationPlayState='running';
+    document.getElementById('stone').style.animationPlayState='running';
+}
 function hideFish(event) {
     event.target.animate([{opacity:1,display:'block'},{opacity:0,display:'none'}],{duration:500,fill:'both',easing:'linear'});
     setTimeout(function () {
